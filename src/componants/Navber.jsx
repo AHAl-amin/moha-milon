687f20d7605352ffabc2d5e889464b96daa8fe3f
Alamin <mdalaminhossain573190@gmail.com>
2024-04-02 01:04:06 +0600

    3th commit

--- src/componants/Navber.jsx
+++ src/componants/Navber.jsx
@@ -1,13 +1,25 @@
-import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 
 const Navber = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const handlelogOut = () => {
+            logOut()
+            .then(() => {
+                console.log('user logOut succesfully');
 
-    const navlinks =<>
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+    const navlinks = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
         <li><NavLink to="/register">Register</NavLink></li>
-    
+
     </>
     return (
         <div className="navbar bg-base-100">
@@ -17,25 +29,39 @@ const Navber = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                     
-                     {
-                        navlinks
-                     }
+
+                        {
+                            navlinks
+                        }
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                 
-                 {
-                    navlinks
-                 }
+
+                    {
+                        navlinks
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                {
+                    user ?
+                        <>
+                            <span>{user.email}</span>
+                            <a onClick={handlelogOut} className="btn">sign out</a>
+                        </>
+                        :
+                        <Link to="/login">
+                            <div className="navbar-end">
+
+                            <button  className="btn ">login </button>
+                            </div>
+                        </Link>
+                }
             </div>
+
         </div>
     );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,8 @@
-import {  createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext, useState } from "react";
+import {  createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types'; // 
 import auth from "../firebase/firebase.config";
+import Login from "../componants/Login";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({children}) => {
@@ -10,7 +11,24 @@ const AuthProvider = ({children}) => {
     const creatUser = (email , password) =>{
         return createUserWithEmailAndPassword(auth, email ,password)
     }
-    const authInfo ={user ,creatUser}
+    const signInUser =(email , password) =>{
+        return signInWithEmailAndPassword(auth ,email ,password)
+    }
+    const logOut =(email , password) =>{
+        return signOut(auth)
+    }
+
+     useEffect(() =>{
+     const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
+         console.log('observing current user inside useeffect', currentUser)
+         setUser (currentUser);
+        });
+        return () =>{
+            unSubscribe();
+        }
+     
+    },[])
+    const authInfo ={user ,creatUser ,signInUser,logOut}
     return (
         <AuthContext.Provider value={authInfo }>
             {children}
